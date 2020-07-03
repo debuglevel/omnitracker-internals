e63614511dbe2e5530df6de51f1f8771034b405e
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-07-03 09:55:42 +0200

    Add fields

---
+++ src/main/kotlin/de/debuglevel/omnitrackerinternals/field/FieldController.kt
@@ -0,0 +1,17 @@
+package de.debuglevel.omnitrackerinternals.field
+
+import de.debuglevel.omnitrackerdatabasebinding.field.Field
+import de.debuglevel.omnitrackerinternals.entity.EntityController
+import io.micronaut.http.annotation.Controller
+import io.swagger.v3.oas.annotations.tags.Tag
+import mu.KotlinLogging
+
+@Controller("/fields")
+@Tag(name = "fields")
+class FieldController(
+    override val entityService: FieldService
+) :
+    EntityController<Field>() {
+    private val logger = KotlinLogging.logger {}
+
+}
---
+++ src/main/kotlin/de/debuglevel/omnitrackerinternals/field/FieldService.kt
@@ -0,0 +1,13 @@
+package de.debuglevel.omnitrackerinternals.field
+
+import de.debuglevel.omnitrackerdatabasebinding.field.Field
+import de.debuglevel.omnitrackerinternals.entity.EntityService
+import mu.KotlinLogging
+import javax.inject.Singleton
+
+@Singleton
+class FieldService(
+    override val entityService: de.debuglevel.omnitrackerdatabasebinding.field.FieldService
+) : EntityService<Field>() {
+    private val logger = KotlinLogging.logger {}
+}
