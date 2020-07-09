7ae19e48a0aceb98a4ff57bbc0096d8ec45449fa
Marc Kohaupt <debuglevel@gmail.com>
2020-07-09 21:57:17 +0200

    Provide layout file download

--- src/main/kotlin/de/debuglevel/omnitrackerinternals/layout/LayoutController.kt
+++ src/main/kotlin/de/debuglevel/omnitrackerinternals/layout/LayoutController.kt
@@ -2,7 +2,10 @@ package de.debuglevel.omnitrackerinternals.layout
 
 import de.debuglevel.omnitrackerdatabasebinding.layout.Layout
 import de.debuglevel.omnitrackerinternals.entity.EntityController
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.server.types.files.StreamedFile
 import io.swagger.v3.oas.annotations.tags.Tag
 import mu.KotlinLogging
 
@@ -14,4 +17,14 @@ class LayoutController(
     EntityController<Layout>() {
     private val logger = KotlinLogging.logger {}
 
+    @Get("/files/{id}")
+    fun getLayoutFile(id: Int): StreamedFile {
+        logger.debug("Called getLayoutFile()")
+
+        val layout = entityService.get(id)
+        val inputStream = layout!!.reportData.inputStream()
+        val fileExtension = layout.outputType?.fileExtension ?: ".unknown"
+        val filename = "${layout.name}.${fileExtension}"
+        return StreamedFile(inputStream, MediaType.TEXT_PLAIN_TYPE).attach(filename)
+    }
 }
