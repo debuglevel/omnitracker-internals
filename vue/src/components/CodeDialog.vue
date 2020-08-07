2c9c7f6f14d2578162b889b1591981507c07fa5e
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-08-07 11:11:16 +0200

    Show script content in a dialog

---
+++ vue/src/components/CodeDialog.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-dialog v-model="dialog">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          v-bind="attrs"
+          v-on="on"
+        >
+          Show code
+        </v-btn>
+      </template>
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{title}}</span>
+        </v-card-title>
+        <v-card-text><pre>{{code}}</pre></v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn text @click="dialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "CodeDialog",
+  props: {
+    title: {},
+    code: {}
+  },
+  data () {
+      return {
+        dialog: false,
+      }
+    },
+};
+</script>
--- vue/src/components/ScriptItem.vue
+++ vue/src/components/ScriptItem.vue
@@ -4,14 +4,19 @@
     <td>{{ script.name }}</td>
     <td>{{ script.type }}</td>
     <td>
-      <pre>{{ script.content }}</pre>
+      <code-dialog v-bind:title="script.folder.path+': '+script.name" v-bind:code="script.content" />
     </td>
   </tr>
 </template>
 
 <script>
+import CodeDialog from "@/components/CodeDialog.vue";
+
 export default {
   name: "ScriptItem",
+    components: {
+    CodeDialog
+  },
   props: {
     script: {}
   }
