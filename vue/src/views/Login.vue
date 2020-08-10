1fa48bd003108e120037931a8514507b00eb8bef
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-08-10 15:12:58 +0200

    Style login page

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,19 +1,33 @@
 <template>
     <div>
-        <h1>Login</h1>
-        <v-card>
-            <v-card-text>
-                This only stores the credentials in the Browser local storage.
-                On each HTTP request, they are included as HTTP basic authentication.
-                As the Browser local storage is insecure, <em>credentials should be unset when work is done</em>.
-            </v-card-text>
-        </v-card>
-        <v-form>
-            <v-text-field v-model="username" label="Username" required></v-text-field>
-            <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
-            <v-btn @click="login">Set credentials</v-btn>
-            <v-btn @click="logout">Unset credentials</v-btn>
-        </v-form>
+
+        <!-- Source: https://vuetifyjs.com/en/examples/layouts/centered/ -->
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+              <v-card-title>Login</v-card-title>
+                <v-card-subtitle>
+                  This only stores the credentials in the Browser local storage.
+                  On each HTTP request, they are included as HTTP basic authentication.
+                  As the Browser local storage is insecure, <strong>credentials should be unset when work is done</strong>.
+                </v-card-subtitle>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field v-model="username" label="Login" prepend-icon="mdi-account" type="text" required />
+                    <v-text-field v-model="password" label="Password" prepend-icon="mdi-lock" type="password" required />
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn @click="login" color="primary">Set credentials</v-btn>
+                  <v-btn @click="logout">Unset credentials</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+
     </div>
 </template>
 
