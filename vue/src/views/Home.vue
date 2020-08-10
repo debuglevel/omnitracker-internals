8dd88e3732a07da52dee7990c86767df52ab636a
Marc Kohaupt <marc.kohaupt@avm-d.de>
2020-08-10 15:00:10 +0200

    Notice about logging in on Home page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <HelloWorld msg="OMNITRACKER Internals" />
+    <v-card class="mx-auto" max-width="344">
+      <v-card-text>Ensure to set your credentials before taking any actions.</v-card-text>
+      <v-card-actions>
+        <v-btn to="/login" text>Login</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
